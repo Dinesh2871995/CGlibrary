8ae8af87e779543c272d813a9fb2d91079e02df4
DineshC <dinesh.c@cloudganga.com>
2018-06-19 14:03:34 +0530

    Initial Commit

---
+++ CGlibrary/CGlibrary.h
@@ -0,0 +1,19 @@
+//
+//  CGlibrary.h
+//  CGlibrary
+//
+//  Created by apple on 19/06/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CGlibrary.
+FOUNDATION_EXPORT double CGlibraryVersionNumber;
+
+//! Project version string for CGlibrary.
+FOUNDATION_EXPORT const unsigned char CGlibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CGlibrary/PublicHeader.h>
+
+
